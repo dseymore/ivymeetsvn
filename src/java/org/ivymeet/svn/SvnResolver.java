c818a760fd8da4b79802d6bf0e04d4cf25f300d3
d.seymore <d.seymore@fdec7cc0-934c-0410-8ff0-b5a90062df18>
2008-05-07 19:23:50 +0000

    starting to do something... hitting ivyde problems..

--- src/java/org/ivymeet/svn/SvnResolver.java
+++ src/java/org/ivymeet/svn/SvnResolver.java
@@ -1,8 +1,14 @@
 package org.ivymeet.svn;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 import org.tmatesoft.svn.core.internal.io.dav.DAVRepositoryFactory;
 
 import fr.jayasoft.ivy.resolver.RepositoryResolver;
+import fr.jayasoft.ivy.util.Message;
 
 /**
  * 
@@ -12,6 +18,7 @@ public class SvnResolver extends RepositoryResolver {
 
 	private String username;
 	private String password;
+	private String secure;
 
 	public SvnResolver() {
 		// get the dav stuff ready to go
@@ -40,5 +47,44 @@ public class SvnResolver extends RepositoryResolver {
 		this.password = password;
 	}
 
+	public String getSecure() {
+		return secure;
+	}
 
+	public void setSecure(String secure) {
+		this.secure = secure;
+		if (secure != null && !"".equalsIgnoreCase(secure)){
+			//see if the file exists and has username and password already
+			File localSecurity = new File(System.getProperty("user.home") + "/.ivyMeetSvn-" + secure);
+			if (!localSecurity.exists()){
+				try{
+					localSecurity.createNewFile();
+				}catch(Exception e){
+					Message.error("Unable to write to user's home directory to create a security file.");
+					throw new RuntimeException("You have to have the ability to write to your home directory.");
+				}
+				String username = "";
+				String password = "";
+				try {
+					System.out.println("Please enter your SVN username: (will only happen once)");
+					BufferedReader reader = new BufferedReader(new InputStreamReader (System.in));
+					username  = reader.readLine();
+					reader.close();
+					System.out.println("Please enter your SVN password: (will only happen once)");
+					reader = new BufferedReader(new InputStreamReader (System.in));
+					password  = reader.readLine();
+					reader.close();
+				} catch (IOException e) {				
+					throw new RuntimeException("Cannot ask for input.");
+				}
+				String alltogether = username + ":" + password;
+				
+				
+				
+			}
+			
+			
+		}
+	}
+	
 }
